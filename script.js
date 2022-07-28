561acfd0a66ca554c5096074e7cb16589117c02a
garrett <gnelson@duck.com>
2022-07-28 11:02:53 -0400

    refactored operation functions and implemented backspace functionality

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const defaultOperation = { a: "", b: "", operator: "" };
+const defaultOperation = { a: "", b: "", operator: "", result: "" };
 
 let previousOperation = { ...defaultOperation };
 let currentOperation = { ...defaultOperation };
@@ -9,13 +9,23 @@ let currentOperation = { ...defaultOperation };
 const currentOperationDisplay = document.querySelector(".current");
 const previousOperationDisplay = document.querySelector(".previous");
 
-const btns = [...document.querySelectorAll("button")];
+const btnClear = document.querySelector("button.clear");
+const btnBackspace = document.querySelector("button.backspace");
+const btnCalcluate = document.querySelector("button.calculate");
+const btnsNumeric = [...document.querySelectorAll("button.numeric")];
+const btnsOperator = [...document.querySelectorAll("button.operator")];
 
 // math operations
 const add = (a, b) => +a + +b;
 const subtract = (a, b) => +a - +b;
 const multiply = (a, b) => +a * +b;
-const divide = (a, b) => +a / +b;
+const divide = (a, b) => {
+  if (+b === 0) {
+    alert("What are you thinking?! You cannot divide by zero!");
+    return;
+  }
+  return +a / +b;
+};
 
 const operate = ({ a, b, operator }) => {
   switch (operator) {
@@ -30,24 +40,29 @@ const operate = ({ a, b, operator }) => {
   }
 };
 
-// ui update functions
-function currentOperationIsEmpty() {
-  const { a, b, operator, result } = currentOperation;
+// helper functions
+function operationIsEmpty(operation) {
+  const { a, b, operator, result } = operation;
   if (!a && !b && !operator && !result) return true;
   return false;
 }
 
+function isValidOperation(operation) {
+  const { a, b, operator } = operation;
+  if (!a || !b || !operator) return false;
+  return true;
+}
+
 function clearOperations() {
-  if (currentOperationIsEmpty()) previousOperation = { ...defaultOperation };
+  // this makes it so you have to click clear twice to remove the previous operation as well
+  if (operationIsEmpty(currentOperation))
+    previousOperation = { ...defaultOperation };
   else currentOperation = { ...defaultOperation };
 }
 
 function createDisplayString(operation) {
-  return (
-    `${operation.a} ${operation.operator} ${operation.b} ${
-      operation.result ? `= ${operation.result}` : ""
-    }`.trim() || " "
-  );
+  const { a, b, operator, result } = operation;
+  return result ? result : `${a} ${operator} ${b}`.trim();
 }
 
 function updateDisplay() {
@@ -55,37 +70,82 @@ function updateDisplay() {
   currentOperationDisplay.textContent = createDisplayString(currentOperation);
 }
 
-function updateCurrent(value, operator) {
-  if (operator) {
-    currentOperation.operator = value;
-    return;
-  } else if (currentOperation.operator) {
+function updateCurrent(value, operator = false) {
+  // when the current operation has a result, any numeric or operator button will trigger a new operation
+  if (currentOperation.result) {
+    previousOperation = { ...currentOperation };
+    currentOperation = { ...defaultOperation };
+
+    if (operator) currentOperation.a = previousOperation.result;
+  }
+
+  // if the button is an operator, the current operation does not have a result, and it is a valid operation, calculate it and start a new operation with the result
+  if (operator && isValidOperation(currentOperation)) {
+    currentOperation.result = operate(currentOperation);
+    previousOperation = { ...currentOperation };
+    currentOperation = { ...defaultOperation };
+    currentOperation.a = previousOperation.result;
+  } else if (
+    operator &&
+    previousOperation.result &&
+    operationIsEmpty(currentOperation)
+  ) {
+    currentOperation.a = previousOperation.result;
+  }
+
+  if (!operator && currentOperation.operator) {
+    if (value === "." && currentOperation.b.includes(".")) return;
     currentOperation.b += value;
+  } else if (operator) {
+    currentOperation.operator = value;
   } else {
+    if (value === "." && currentOperation.a.includes(".")) return;
     currentOperation.a += value;
   }
 }
 
 // event listener callback functions
-function handleButtonClick(e) {
+function handleNumericClick(e) {
   const value = e.target.dataset.key;
-  const operator = e.target.classList.contains("operator");
+  updateCurrent(value);
+  updateDisplay();
+}
 
-  if (value === "clear") {
-    clearOperations();
-  } else if (currentOperation.result) {
-    previousOperation = { ...currentOperation };
-    currentOperation = { ...defaultOperation };
-    updateCurrent(value, operator);
-  } else if (value === "=") {
-    const result = operate(currentOperation);
-    currentOperation = { ...currentOperation, result };
-  } else {
-    updateCurrent(value, operator);
+function handleOperatorClick(e) {
+  const value = e.target.dataset.key;
+  updateCurrent(value, true);
+  updateDisplay();
+}
+
+function handleCalculateClick() {
+  if (!isValidOperation(currentOperation)) return;
+  currentOperation.result = +operate(currentOperation).toFixed(5);
+  updateDisplay();
+}
+
+function handleClearClick() {
+  clearOperations();
+  updateDisplay();
+}
+
+function handleBackspaceClick() {
+  const { a, b, operator, result } = currentOperation;
+  if (b) {
+    if (result) currentOperation.result = "";
+    currentOperation.b = b.slice(0, b.length - 1);
+  } else if (operator) {
+    currentOperation.operator = "";
+  } else if (a) {
+    currentOperation.a = a.slice(0, a.length - 1);
   }
-  console.log(currentOperation);
   updateDisplay();
 }
 
 // event listeners
-btns.forEach((btn) => btn.addEventListener("click", handleButtonClick));
+btnsNumeric.forEach((btn) => btn.addEventListener("click", handleNumericClick));
+btnsOperator.forEach((btn) =>
+  btn.addEventListener("click", handleOperatorClick)
+);
+btnCalcluate.addEventListener("click", handleCalculateClick);
+btnClear.addEventListener("click", handleClearClick);
+btnBackspace.addEventListener("click", handleBackspaceClick);
